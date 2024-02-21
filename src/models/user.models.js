15a8efb1e2d44fa091fac8c81fc6d97280d5ee6f
dubey-Ji <ashd1014@hotmail.com>
2024-02-21 17:57:27 +0530

    added pre methods and some other methods in user model

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,6 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const addressSchema = mongoose.Schema({
   flatNo: {
@@ -32,8 +34,48 @@ const userSchema = mongoose.Schema(
       required: true,
     },
     address: addressSchema,
+    refreshToken: {
+      type: String,
+      default: null,
+    },
   },
   { timeStamps: true }
 );
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = bcrypt.hash(this.password, 10);
+  return next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      phoneNumber: this.phoneNumber,
+    },
+    process.env.ACCESS_TOKEN_KEY,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRE_TIME,
+    }
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_KEY,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRE_TIME,
+    }
+  );
+};
 
 export const User = mongoose.model('User', userSchema);
