ec24bfe13cb63e04438ae3d9adbf6c8f1af929ae
dubey-Ji <ashd1014@hotmail.com>
2024-02-21 17:05:42 +0530

    added utils

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,17 @@
+// async await
+// const asyncHandler = (requestHandler) => async (req, res, next) => {
+//   try {
+//     await requestHandler(req, res, next);
+//   } catch (err) {
+//     res.status(err.code || 500).json({ success: false, message: err.message});
+//   }
+// };
+
+// Promise
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
